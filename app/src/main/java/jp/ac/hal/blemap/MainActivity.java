13670fd7bff79b3af19446c2d469cb54840d2120
pasco3596 <halhogechi@gmail.com>
2016-11-18 23:06:14 +0900

    文字化け調査

--- app/src/main/java/jp/ac/hal/blemap/MainActivity.java
+++ app/src/main/java/jp/ac/hal/blemap/MainActivity.java
@@ -1,6 +1,8 @@
 package jp.ac.hal.blemap;
 
 import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
@@ -10,7 +12,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
@@ -29,11 +34,15 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
     public static final String IBEACON_FORMAT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24";
 
     private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+    private static final int BLUETOOTH_ENABLE = 1;
+
+    private BluetoothAdapter bluetoothAdapter;
 
     ListView lv;
     List<MyBeacon> allList;
     List<MyBeacon> alist;
-
+    TextView tv;
+    Button bt;
     Handler handler = new Handler();
 
     @Override
@@ -41,11 +50,11 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},PERMISSION_REQUEST_COARSE_LOCATION);
-            }
-        }
+
+        tv =(TextView)findViewById(R.id.textView);
+
+        bt = (Button)findViewById(R.id.button);
+        bt.setOnClickListener(view -> tv.setText("あああああ"));
 
         MyDatabaseHelper mh = new MyDatabaseHelper(this);
         SQLiteDatabase db = mh.getWritableDatabase();
@@ -144,8 +153,9 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
     @Override
     protected void onResume() {
         super.onResume();
+        requestPermission();
+       // bluetoothOn();
         beaconManager.bind(this);
-
     }
 
     @Override
@@ -153,4 +163,43 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
         beaconManager.unbind(this);
         super.onPause();
     }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        if (requestCode == PERMISSION_REQUEST_COARSE_LOCATION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this, "許可したで", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "許可せんとできんで", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+        }
+    }
+
+    public void bluetoothOn() {
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter.isEnabled()) {
+            Intent btOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(btOn, BLUETOOTH_ENABLE);
+        }
+    }
+
+    public void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},PERMISSION_REQUEST_COARSE_LOCATION);
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == BLUETOOTH_ENABLE) {
+                Toast.makeText(MainActivity.this, R.string.aa, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
